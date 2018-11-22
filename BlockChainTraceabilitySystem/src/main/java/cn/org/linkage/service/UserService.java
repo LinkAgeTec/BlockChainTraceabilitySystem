bc32827b2da864ead14c9df4773215a8cad193f1
ACtangle <674253854@qq.com>
2018-11-22 12:03:32 +0800

    User Dao

---
+++ BlockChainTraceabilitySystem/src/main/java/cn/org/linkage/service/UserService.java
@@ -0,0 +1,30 @@
+package cn.org.linkage.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+* @Description:    User Dao类
+* @Author:         ACtangle
+* @CreateDate:     2018/11/22 11:53
+* @UpdateUser:     ACtangle
+* @UpdateDate:     2018/11/22 11:53
+* @UpdateRemark:   修改内容
+* @Version:        1.0
+*/
+public class UserService implements UserDetailsService{
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        /**
+         *功能描述
+         * 实现了UserDetailsService的唯一实现方法loadUserByUsername，用于数据库查询user。
+                * @author ACtangle
+                * @date 2018/11/22
+                * @param [s]
+                * @return org.springframework.security.core.userdetails.UserDetails
+                */
+        return null;
+    }
+}
